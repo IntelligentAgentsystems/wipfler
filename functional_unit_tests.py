a4d7625e53f373fead5d4011458f04de981d924f
Elias Wipfler <elias.wipfler@outlook.com>
2019-11-21 17:01:03 +0100

    conveyor working

---
+++ functional_unit_tests.py
@@ -0,0 +1,42 @@
+import unittest
+
+from functional_unit import *
+from sheet import Sheet
+
+
+class FunctionalUnitTest(unittest.TestCase):
+
+    def test_hand_to(self):
+        fu1 = FunctionalUnit((0, 0))
+        fu2 = FunctionalUnit((0, 1))
+        fu1.sheet = Sheet()
+
+        fu1.give_sheet_to(fu2)
+
+    def test_hand_to_out_of_range(self):
+        with self.assertRaises(UnitOutOfRangeError):
+            fu1 = FunctionalUnit((0, 0))
+            fu2 = FunctionalUnit((0, 2))
+            fu1.sheet = Sheet()
+
+            fu1.give_sheet_to(fu2)
+
+    def test_hand_to_no_sheet(self):
+        with self.assertRaises(NoSheetError):
+            fu1 = FunctionalUnit((0, 0))
+            fu2 = FunctionalUnit((0, 1))
+
+            fu1.give_sheet_to(fu2)
+
+    def test_hand_to_no_uccupied(self):
+        with self.assertRaises(UnitOccupiedError):
+            fu1 = FunctionalUnit((0, 0))
+            fu2 = FunctionalUnit((0, 1))
+            fu1.sheet = Sheet()
+            fu2.sheet = Sheet()
+
+            fu1.give_sheet_to(fu2)
+
+
+if __name__ == '__main__':
+    unittest.main()
